45c4142111a604a756fca6d179dd31cab650f4f9
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-10-02 06:32:43 +0200

    MissingNumber with bitmanipulation

--- Algorithms/easy/MissingNumber.java
+++ Algorithms/easy/MissingNumber.java
@@ -13,3 +13,14 @@ class Solution {
         return maxNumber;
     }
 }
+
+// with bitmanipulation *revisit*
+class Solution {
+    public int missingNumber(int[] nums) {
+        int x = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            x ^= i ^ nums[i];
+        }
+        return x;
+    }
+}
